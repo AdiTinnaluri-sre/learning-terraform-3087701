3dbd8f856aede2132cbef044fc0e9c3fa8306bfb
tans trainings <tans.trainings@gmail.com>
2025-07-03 14:44:16 +0100

    added new dev vpc

--- main.tf
+++ main.tf
@@ -19,11 +19,29 @@ data "aws_ami" "app_ami" {
   }
 
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
@@ -35,50 +53,12 @@ module "blog_sg" {
   version = "5.3.0"
   name = "blog"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 
-
   }
-
-
-resource "aws_security_group" "blog" {
-  name = "blog_sg"
-  vpc_id = data.aws_vpc.default.id
-  description = "Allow http and https traffic. Allow traffic from anywhere"
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"] # Allow HTTP traffic from anywhere
-  
-  security_group_id = aws_security_group.blog.id
-  }
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type = "ingress"
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"] # Allow HTTPS traffic from anywhere
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_egrees_out" {
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1" # Allow all outbound traffic
-  cidr_blocks = ["0.0.0.0/0"] # Allow HTTPS traffic from anywhere
-
-  security_group_id = aws_security_group.blog.id
-}
 
